68b01d82d74e366638a3f1227511c56be11ace2f
weslley <weslleylc@gmail.com>
2020-06-20 19:50:31 +0000

    add settings

--- project_portfolio/settings.py
+++ project_portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'sba4wi@7uhzj$t=p)c&vho45mfwkx)17d+=(@=ilra=8=bz3s3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["weslleylc.pythonanywhere.com"]
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
